664194116bf105bd0c7fc93d8dfe290295d37d77
Mark Gardner <themarkgardner@gmail.com>
2024-01-15 15:06:42 -0600

    started animations with gsap and project cards

--- app/components/ProjectsContainer/projectsContainer.js
+++ app/components/ProjectsContainer/projectsContainer.js
@@ -3,19 +3,42 @@
 import styles from './projectsContainer.module.scss'
 import {projectData} from '../../utils/projectData'
 import ProjectCard from '../ProjectCard/ProjectCard'
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 
 export default function ProjectsContainer() {
     const projectTitleRef=useRef();
+    const projectRefs = useRef([]);
+
 
     useLayoutEffect (() => {      
-    // const projectsTitleHeight =  projectTitleRef.current.offsetHeight;
-    // document.documentElement.style.setProperty("--projectsTitleHeight", projectsTitleHeight + "px");
     const projectsContainerOffset =  projectTitleRef.current.getBoundingClientRect();
     document.documentElement.style.setProperty("--projectsContainerOffset", (projectsContainerOffset.top +100) + "px");
     
     }
     )
+    
+    useGSAP (() => {
+        projectData.forEach((project) => {
+    
+        let tl =gsap.timeline({
+            scrollTrigger: {
+                scrollTrigger:projectRefs.current,
+                scroller:'.projectsScroller',
+                markers:true,
+                scrub: true,
+            },
+            
+            
+        })
+            tl.fromTo(projectRefs.current[project.displayClass], {opacity:0}, {opacity:1})
+    })
+
+})
+
+
     
     return (
         <>
@@ -28,7 +51,7 @@ export default function ProjectsContainer() {
                 <div className={styles.projectsScroller}>
                     <div className={styles.projectCardSpacer}></div>
                     {projectData?.map((project) =>
-                        <ProjectCard projectData={project} />
+                        <ProjectCard projectData={project} key={project.displayClass} ref={el => projectRefs.current[project.displayClass]=el}/>
                     )
                     }
                     <div className={styles.projectCardSpacer}></div>
