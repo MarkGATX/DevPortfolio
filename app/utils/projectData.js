86f62c9322b36373ef826992056531aea9261f86
Mark Gardner <themarkgardner@gmail.com>
2025-05-29 14:51:44 -0500

    added typescript image and to playdate

--- app/utils/projectData.js
+++ app/utils/projectData.js
@@ -44,7 +44,7 @@ export const projectData = [
             "firebase",
             "vercel",
             "sass",
-            "javascript",
+            "typescript",
             "postgresql",                     
             "css3",
             "googleFonts",
